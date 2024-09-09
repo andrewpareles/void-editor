680ced201f94b1297f9fd76c9cda58611579bc35
Andrew <andrewpareles@gmail.com>
2024-09-08 21:34:52 -0700

    add extension to dirs.js

--- build/npm/dirs.js
+++ build/npm/dirs.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 
 // Complete list of directories where yarn should be executed to install node modules
 const dirs = [
+	'extensions/helloworld', // <-- glass
+
 	'',
 	'build',
 	'extensions',
